0049d46dd1b2dc4425c46c623ff68df60c0fc686
Don Kelly <karfai@gmail.com>
2013-10-15 00:49:22 +0400

    factor queries

---
+++ mongo-ruby/queries.rb
@@ -0,0 +1,59 @@
+require 'mongo'
+
+include Mongo
+
+module Queries
+  def self.mailboxes(cl)
+    cl.db['mail']['mailboxes']
+  end
+
+  def self.folder_contents(cl, user, folder)
+    mailboxes(cl).find({ :user => user, :name => folder}).each() do |doc|
+      puts "#{doc['name']}:"
+      message_no = 1
+      doc['messages'].each() do |message|
+        puts "#{message_no}: #{message['Subject']}"
+        message_no += 1
+      end
+    end
+  end
+
+  def self.folders(cl, user)
+    mailboxes(cl).find({'user' => user}).each() do |doc|
+      puts "(#{doc['messages'].length}) #{doc['name']}"
+    end
+  end
+
+  def self.user_information_nested(cl)
+    counts = {}
+    mailboxes = mailboxes(cl)
+    folder_counts = mailboxes.aggregate([
+                                         { '$group' =>
+                                           {
+                                             '_id' => '$user',
+                                             'count' => { '$sum' => 1 },
+                                           },
+                                         },
+                                        ])
+    folder_counts.each() do |user_counts|
+      counts[user_counts['_id']] = { :folders => user_counts['count'] }
+    end
+
+    message_counts = mailboxes.aggregate([
+                                          { '$unwind' => '$messages'},
+                                          { '$group' =>
+                                            {
+                                              '_id' => '$user',
+                                              'count' => { '$sum' => 1 },
+                                            },
+                                          },
+                                         ])
+    message_counts.each() do |user_counts|
+      counts[user_counts['_id']][:messages] = user_counts['count']
+    end
+
+    counts.each() do |k, v|
+      puts "#{k}: #{v[:folders]} folders, #{v[:messages]} messages"
+    end
+  end
+end
--- mongo-ruby/query.nested.rb
+++ mongo-ruby/query.nested.rb
@@ -1,67 +1,16 @@
 require 'mongo'
+require './queries'
 
 include Mongo
-
-def folder_contents(mailboxes, user, folder)
-  mailboxes.find({ :user => user, :name => folder}).each() do |doc|
-    puts "#{doc['name']}:"
-    message_no = 1
-    doc['messages'].each() do |message|
-      puts "#{message_no}: #{message['Subject']}"
-      message_no += 1
-    end
-  end
-end
-
-def folders(mailboxes, user)
-  mailboxes.find({'user' => user}).each() do |doc|
-    puts "(#{doc['messages'].length}) #{doc['name']}"
-  end
-end
-
-def users(mailboxes)
-  counts = {}
-  folder_counts = mailboxes.aggregate([
-                                       { '$group' =>
-                                         {
-                                           '_id' => '$user',
-                                           'count' => { '$sum' => 1 },
-                                         },
-                                       },
-                                      ])
-  folder_counts.each() do |user_counts|
-    counts[user_counts['_id']] = { :folders => user_counts['count'] }
-  end
-
-  message_counts = mailboxes.aggregate([
-                                        { '$unwind' => '$messages'},
-                                        { '$group' =>
-                                          {
-                                            '_id' => '$user',
-                                            'count' => { '$sum' => 1 },
-                                          },
-                                        },
-                                       ])
-  message_counts.each() do |user_counts|
-    counts[user_counts['_id']][:messages] = user_counts['count']
-  end
-
-  counts.each() do |k, v|
-    puts "#{k}: #{v[:folders]} folders, #{v[:messages]} messages"
-  end
-end
 
 user = ARGV[0]
 folder = ARGV[1]
 
-cl = MongoClient.new
-db = cl.db['mail']
-
 if user && folder
-  folder_contents(db['mailboxes'], user, folder)
+  Queries.folder_contents(MongoClient.new, user, folder)
 elsif user
-  folders(db['mailboxes'], user)
+  Queries.folders(MongoClient.new, user)
 else
-  users(db['mailboxes'])
+  Queries.user_information_nested(MongoClient.new)
 end
 
